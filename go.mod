85282af5e0a167fb6d943713adcc453f7488b8fc
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2023-08-25 15:00:23 -0500

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,10 @@
+module demo
+
+go 1.14
+
+require (
+	github.com/aws/aws-sdk-go v1.33.21
+	github.com/pkg/errors v0.9.1
+	github.com/square/certigo v1.12.1
+	golang.org/x/text v0.12.0
+)
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "github.com/aws/aws-sdk-go"
+    "github.com/pkg/errors"
+    "golang.org/x/text"
+)
+
+func main() {
+    fmt.Println("Hello, World!")
+}
