f2d57f88808eb5900eabf214676893547a7bd065
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2023-08-25 14:24:38 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "github.com/aws/aws-sdk-go"
+    "github.com/pkg/errors"
+    "github.com/square/certigo"
+    "golang.org/x/text"  
+    "golang.org/x/net"
+)
+
+func main() {
+    fmt.Println("Hello, World!")
+}
